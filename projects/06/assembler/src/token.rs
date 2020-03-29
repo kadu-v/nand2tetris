ba313caab919ec841086253e1206acc852934b3b
ikemorikazuki <kazuonsunday@yahoo.co.jp>
2020-03-29 17:32:02 +0900

    define token

--- projects/06/assembler/src/token.rs
+++ projects/06/assembler/src/token.rs
@@ -1,6 +1,46 @@
 //! Token for Hack assembler
 
 pub enum Token {
-    LParen, // (
-    RParen, // )
+    // A命令のトークン
+    At, // "@"
+
+    Int(String),    // "1234"
+    Symbol(String), // "SUM"
+
+    // C命令のトークン
+    // comp部分のトークン
+    // レジスタ
+    A, // "A"
+    M, // "M"
+    D, // "D"
+
+    // 演算子
+    Plus,  // "+"
+    Minus, // "-"
+    And,   // "&"
+    Or,    // "|"
+    Bang,  // "!"
+
+    // dest部分のトークン
+    Null,
+    MD,
+    AM,
+    AD,
+    AMD,
+
+    // jump部分のトークン
+    JGT,
+    JEQ,
+    JGE,
+    JLT,
+    JNE,
+    JLE,
+    JMP,
+
+    Semi, // ";"
+
+    Coment, // "//"
+
+    LParen, // "("
+    RParen, // ")"
 }
