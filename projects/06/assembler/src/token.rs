fb4fba510d8d865c09ef0e3287ec629156e2b1f1
ikemorikazuki <kazuonsunday@yahoo.co.jp>
2020-03-29 22:57:42 +0900

    define token constructor

--- projects/06/assembler/src/token.rs
+++ projects/06/assembler/src/token.rs
@@ -1,10 +1,12 @@
 //! Token for Hack assembler
 
-pub enum Token {
-    // A命令のトークン
-    At, // "@"
+use crate::loc::*;
 
-    Int(String),    // "1234"
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TokenKind {
+    // A命令のトークン
+    AtSign,         // "@"
+    Number(usize),  // "1234"
     Symbol(String), // "SUM"
 
     // C命令のトークン
@@ -22,7 +24,6 @@ pub enum Token {
     Bang,  // "!"
 
     // dest部分のトークン
-    Null,
     MD,
     AM,
     AD,
@@ -37,10 +38,112 @@ pub enum Token {
     JLE,
     JMP,
 
-    Semi, // ";"
-
-    Coment, // "//"
+    Semicoron, // ";"
 
     LParen, // "("
     RParen, // ")"
+}
+
+pub type Token = Annot<TokenKind>;
+
+impl Token {
+    pub fn at_sign(loc: Loc) -> Self {
+        Self::new(TokenKind::AtSign, loc)
+    }
+
+    pub fn number(n: usize, loc: Loc) -> Self {
+        Self::new(TokenKind::Number(n), loc)
+    }
+
+    pub fn symbol(s: String, loc: Loc) -> Self {
+        Self::new(TokenKind::Symbol(s), loc)
+    }
+
+    pub fn a(loc: Loc) -> Self {
+        Self::new(TokenKind::A, loc)
+    }
+
+    pub fn d(loc: Loc) -> Self {
+        Self::new(TokenKind::D, loc)
+    }
+
+    pub fn m(loc: Loc) -> Self {
+        Self::new(TokenKind::M, loc)
+    }
+
+    pub fn ad(loc: Loc) -> Self {
+        Self::new(TokenKind::AD, loc)
+    }
+
+    pub fn am(loc: Loc) -> Self {
+        Self::new(TokenKind::AM, loc)
+    }
+
+    pub fn md(loc: Loc) -> Self {
+        Self::new(TokenKind::MD, loc)
+    }
+
+    pub fn amd(loc: Loc) -> Self {
+        Self::new(TokenKind::AMD, loc)
+    }
+
+    pub fn plus(loc: Loc) -> Self {
+        Self::new(TokenKind::Plus, loc)
+    }
+
+    pub fn minus(loc: Loc) -> Self {
+        Self::new(TokenKind::Minus, loc)
+    }
+
+    pub fn and(loc: Loc) -> Self {
+        Self::new(TokenKind::And, loc)
+    }
+
+    pub fn or(loc: Loc) -> Self {
+        Self::new(TokenKind::Or, loc)
+    }
+
+    pub fn bang(loc: Loc) -> Self {
+        Self::new(TokenKind::Bang, loc)
+    }
+
+    pub fn jgt(loc: Loc) -> Self {
+        Self::new(TokenKind::JGT, loc)
+    }
+
+    pub fn jeq(loc: Loc) -> Self {
+        Self::new(TokenKind::JEQ, loc)
+    }
+
+    pub fn jge(loc: Loc) -> Self {
+        Self::new(TokenKind::JGE, loc)
+    }
+
+    pub fn jlt(loc: Loc) -> Self {
+        Self::new(TokenKind::JLT, loc)
+    }
+
+    pub fn jne(loc: Loc) -> Self {
+        Self::new(TokenKind::JNE, loc)
+    }
+
+    pub fn jle(loc: Loc) -> Self {
+        Self::new(TokenKind::JLE, loc)
+    }
+
+    pub fn jmp(loc: Loc) -> Self {
+        Self::new(TokenKind::JMP, loc)
+    }
+
+    pub fn semicoron(loc: Loc) -> Self {
+        Self::new(TokenKind::Semicoron, loc)
+    }
+
+    pub fn lparen(loc: Loc) -> Self {
+        Self::new(TokenKind::LParen, loc)
+    }
+
+    pub fn rpare(loc: Loc) -> Self {
+        Self::new(TokenKind::RParen, loc)
+    }
 }
