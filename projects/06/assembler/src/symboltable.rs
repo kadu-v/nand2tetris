811edd125bcafb0b30740736b72a3279decc06dd
ikemorikazuki <kazuonsunday@yahoo.co.jp>
2020-04-01 16:46:33 +0900

    finish SymbolTable

--- projects/06/assembler/src/symboltable.rs
+++ projects/06/assembler/src/symboltable.rs
@@ -6,16 +6,42 @@ use std::collections::HashMap;
 /// シンボルテーブルを表す構造体
 #[derive(Debug, PartialEq, Eq)]
 pub struct SymbolTable<'a> {
-    table: HashMap<&'a TokenKind, u16>,
-    address: u16,
+    table: HashMap<&'a str, u16>,
 }
 
 impl<'a> SymbolTable<'a> {
     pub fn new() -> SymbolTable<'a> {
         SymbolTable {
             table: HashMap::new(),
-            address: 0, // 次の命令のアドレス
         }
+    }
+
+    pub fn add_entry(&mut self, symbol: &'a str, address: u16) {
+        self.table.entry(symbol).or_insert(address);
+    }
+
+    pub fn get_address(&self, symbol: &'a str) -> Option<&u16> {
+        self.table.get(symbol)
+    }
+
+    pub fn make(tokens: &Vec<Vec<Token>>) -> SymbolTable {
+        let mut table = SymbolTable::new();
+        let mut address = 0;
+        for line in tokens {
+            if line.len() < 2 {
+                address += 1;
+                continue;
+            }
+            match (line[0].get_value(), line[1].get_value()) {
+                (TokenKind::LParen, TokenKind::Symbol(s)) => {
+                    table.add_entry(s, address);
+                }
+                _ => {
+                    address += 1;
+                }
+            }
+        }
+        table
     }
 }
 
@@ -30,7 +56,6 @@ fn test_new() {
     assert_eq!(
         SymbolTable {
             table: HashMap::new(),
-            address: 0
         },
         SymbolTable::new()
     );
@@ -38,7 +63,8 @@ fn test_new() {
 
 #[test]
 fn test_symbol_table() {
-    let input = "@R0
+    use crate::lexer::lexer::*;
+    let mut input = "@R0
         D=M              
         @R1
         D=D-M            
@@ -59,4 +85,15 @@ fn test_symbol_table() {
         0;JMP           
         "
     .as_bytes();
+    let expect = [
+        ("OUTPUT_FIRST", 0x000a),
+        ("OUTPUT_D", 0x000c),
+        ("INFINITE_LOOP", 0x000e),
+    ];
+
+    let tokens = lex_all(&mut input).unwrap();
+    let table = SymbolTable::make(&tokens);
+    for (key, value) in expect.iter() {
+        assert_eq!(value, table.get_address(key).unwrap());
+    }
 }
