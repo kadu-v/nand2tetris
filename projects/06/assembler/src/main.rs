a2aa0f3bf0761f5421c4d99bb220a4561c82c65c
ikemorikazuki <kazuonsunday@yahoo.co.jp>
2020-04-01 15:39:35 +0900

    add sample

--- projects/06/assembler/src/main.rs
+++ projects/06/assembler/src/main.rs
@@ -1,3 +1,67 @@
+use assembler::lexer::lexer::lex_all;
+
 fn main() {
-    println!("hello world");
+    let mut input = "@R0
+        D=M              // D = first number
+        @R1
+        D=D-M            // D = first number - second number
+        @OUTPUT_FIRST
+        D;JGT            // if D>0 (first is greater) goto output_first
+        @R1
+        D=M              // D = second number
+        @OUTPUT_D
+        0;JMP            // goto output_d
+      (OUTPUT_FIRST)
+        @R0             
+        D=M              // D = first number
+      (OUTPUT_D)
+        @R2
+         M=D              // M[2] = D (greatest number)
+      (INFINITE_LOOP)
+        @INFINITE_LOOP
+        0;JMP            // infinite loop
+        "
+    .as_bytes();
+
+    println!("In the case of input that succeeds in lexical analysis:\n");
+    match lex_all(&mut input) {
+        Err(lexerr) => println!("{:?}", lexerr),
+        Ok(lines) => {
+            for line in lines {
+                println!("{:?}", line);
+            }
+        }
+    }
+
+    input = "@R0
+        D=M              // D = first number
+        @R1
+        D=D-M            // D = first number - second number
+        @OUTPUT_FIRST
+        D;JGT            / if D>0 (first is greater) goto output_first
+        @R1
+        D=M              // D = second number
+        @OUTPUT_D
+        0;JMP            // goto output_d
+      (OUTPUT_FIRST)
+        @R0             
+        D=M              // D = first number
+      (OUTPUT_D)
+        @R2
+         M=D              // M[2] = D (greatest number)
+      (INFINITE_LOOP)
+        @INFINITE_LOOP
+        0;JMP            // infinite loop
+        "
+    .as_bytes();
+
+    println!("\n\n\nIn case of input that fails lexical analysis:\n");
+    match lex_all(&mut input) {
+        Err(lexerr) => println!("{:?}", lexerr),
+        Ok(lines) => {
+            for line in lines {
+                println!("{:?}", line);
+            }
+        }
+    };
 }
